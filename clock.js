ee73a33c7a2e48098c839d71be00853b1076351d
HS2011 <121203281+HS2011@users.noreply.github.com>
2023-10-01 09:35:24 -0400

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,26 @@
+status = ""
+function preload(){
+imag = loadImage("IMG_1032.jpg")
+}
+function setup(){
+canvas = createCanvas(400,500)
+canvas.position(400,150)
+object = ml5.objectDetector("cocossd", ModelLoaded)
+document.getElementById("status").innerHTML = "Status: Detecting Objects."
+}
+function ModelLoaded() {
+    console.log("Model Loaded")
+    status = true
+    objection.detect(imag, gotResults)
+  
+  }
+  function gotResults(results, error){
+    if(error){
+      console.error(error)
+    }
+    console.log(results)
+    objects = results
+  }
+function draw(){
+
+}
